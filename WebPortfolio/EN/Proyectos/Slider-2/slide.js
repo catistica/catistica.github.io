ef176299e74e88374a44472bf4c1f58e7c384669
catpastorino <catu.pastorino@gmail.com>
2024-04-04 16:25:16 -0300

    Files

---
+++ WebPortfolio/EN/Proyectos/Slider-2/slide.js
@@ -0,0 +1,44 @@
+const slider = document.querySelector('.slider');
+const carousel = document.querySelector('.carousel');
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+var direction;
+
+prev.addEventListener('click', function(){
+    if (direction === -1) {
+        direction = 1;
+        slider.appendChild(slider.firstElementChild);
+    }
+    direction = 1;
+    slider.style.transform = 'translate(50%)';
+    carousel.style.justifyContent = 'flex-end';
+});
+
+next.addEventListener('click', function(){
+    if (direction === 1) {
+        direction = -1;
+        slider.prepend(slider.lastElementChild);
+    }
+
+    direction = -1;
+    slider.style.transform = 'translate(-50%)';
+    carousel.style.justifyContent = 'flex-start';
+});
+
+slider.addEventListener('webkitTransitionEnd', function(){
+    if (direction === -1) {
+        slider.appendChild(slider.firstElementChild);
+    } else if (direction === 1){
+        slider.prepend(slider.lastElementChild);
+    }
+    
+
+    slider.style.transition = 'none'
+    slider.style.transform = 'translate(0)'
+    setTimeout(function(){
+        slider.style.transition = 'all 0.5s'
+
+    });
+})
